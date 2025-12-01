b87887d438fa0b7ec4b853869df734d3d03b6bfe
Matt Krieger <makrieger212@gmail.com>
2025-11-30 21:33:06 -0700

    added auto scroll behavior to jump to top of current carousel card

--- src/components/ProjectCarousel.js
+++ src/components/ProjectCarousel.js
@@ -7,14 +7,17 @@ import "swiper/css/navigation";
 import { useRef } from "react";
 import "../styles/project-carousel.css";
 
-export default function ProjectCarousel({ projects }) {
+export default function ProjectCarousel({ projects, parentRef, carouselRef }) {
   const swiperRef = useRef(null);
 
   const handleSlideChange = () => {
-    const activeSlide = swiperRef.current?.swiper.slides[swiperRef.current.swiper.activeIndex];
-    if (activeSlide) {
-      const scrollable = activeSlide.querySelector(".card-scrollable-content");
-      if (scrollable) scrollable.scrollTop = 0;
+    if (parentRef?.current && carouselRef?.current) {
+      // Calculate carousel's offset relative to parent
+      const parentTop = parentRef.current.getBoundingClientRect().top + parentRef.current.scrollTop;
+      const carouselTop = carouselRef.current.offsetTop;
+
+      // Scroll parent so that carousel aligns to top
+      parentRef.current.scrollTo({ top: carouselTop, behavior: "smooth" });
     }
   };
 
@@ -27,8 +30,8 @@ export default function ProjectCarousel({ projects }) {
         navigation={true}
         spaceBetween={30}
         slidesPerView={1}
-        autoHeight={true}  // dynamically adjust height
-        onSlideChange={handleSlideChange} // reset scroll
+        autoHeight={true}
+        onSlideChangeTransitionStart={handleSlideChange}
       >
         {projects.map((proj, index) => {
           const Component = proj.component;
@@ -48,27 +51,45 @@ export default function ProjectCarousel({ projects }) {
   );
 }
 
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Pagination, Navigation } from "swiper/modules";
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/navigation";
 
-// export default function ProjectCarousel({ projects }) {
+// import { useRef } from "react";
+// import "../styles/project-carousel.css";
+
+// export default function ProjectCarousel({ projects, parentRef }) {
+//   const swiperRef = useRef(null);
+
+//   const handleSlideChange = () => {
+//     // Scroll the parent container to the top of the carousel
+//     if (parentRef?.current) {
+//       parentRef.current.scrollTo({ top: 0, behavior: "smooth" });
+//     }
+//   };
+
 //   return (
 //     <div className="carousel-container">
 //       <Swiper
+//         ref={swiperRef}
 //         modules={[Pagination, Navigation]}
 //         pagination={{ clickable: true }}
 //         navigation={true}
 //         spaceBetween={30}
 //         slidesPerView={1}
-//         autoHeight={true}   // <-- custom fits height of each card
-//         style={{ width: "100%", height: "100%" }}
+//         autoHeight={true}  // dynamically adjust height
+//         onSlideChangeTransitionStart={handleSlideChange} // scroll parent
 //       >
 //         {projects.map((proj, index) => {
-//           const Component = proj.component; // <-- assign to uppercase
+//           const Component = proj.component;
 //           return (
 //             <SwiperSlide key={index}>
 //               <div className="project-card">
 //                 <h2 className="project-title">{proj.title}</h2>
 //                 <div className="card-scrollable-content">
-//                   <Component />   {/* <-- render dynamically */}
+//                   <Component />
 //                 </div>
 //               </div>
 //             </SwiperSlide>
@@ -78,3 +99,52 @@ export default function ProjectCarousel({ projects }) {
 //     </div>
 //   );
 // }
+
+// // import { Swiper, SwiperSlide } from "swiper/react";
+// // import { Pagination, Navigation } from "swiper/modules";
+// // import "swiper/css";
+// // import "swiper/css/pagination";
+// // import "swiper/css/navigation";
+
+// // import { useRef } from "react";
+// // import "../styles/project-carousel.css";
+
+// // export default function ProjectCarousel({ projects }) {
+// //   const swiperRef = useRef(null);
+
+// //   const handleSlideChange = () => {
+// //     const activeSlide = swiperRef.current?.swiper.slides[swiperRef.current.swiper.activeIndex];
+// //     if (activeSlide) {
+// //       activeSlide.scrollTo({ top: 0, behavior: 'smooth' });
+// //     }
+// //   };
+
+// //   return (
+// //     <div className="carousel-container">
+// //       <Swiper
+// //         ref={swiperRef}
+// //         modules={[Pagination, Navigation]}
+// //         pagination={{ clickable: true }}
+// //         navigation={true}
+// //         spaceBetween={30}
+// //         slidesPerView={1}
+// //         autoHeight={true}  // dynamically adjust height
+// //         onSlideChangeTransitionStart={handleSlideChange} // reset scroll
+// //       >
+// //         {projects.map((proj, index) => {
+// //           const Component = proj.component;
+// //           return (
+// //             <SwiperSlide key={index}>
+// //               <div className="project-card">
+// //                 <h2 className="project-title">{proj.title}</h2>
+// //                 <div className="card-scrollable-content">
+// //                   <Component />
+// //                 </div>
+// //               </div>
+// //             </SwiperSlide>
+// //           );
+// //         })}
+// //       </Swiper>
+// //     </div>
+// //   );
+// // }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,18 +1,22 @@
 import '../styles/projects.css';
 import CovidGraphic from '../charts/covid.js';
-// import ProjectCarousel and cards
 import ProjectCarousel from '../components/ProjectCarousel';
 import NewsScraperCard from '../components/cards/NewsScraper';
 import HomeLabCard from '../components/cards/HomeLab';
+import { useRef } from 'react';
 
 export default function Projects() {
-    const projectData = [
+  const projectData = [
     { title: "News Scraper", component: NewsScraperCard },
     { title: "Home Lab Setup", component: HomeLabCard }
   ];
+
+  const firstProjectRef = useRef(null);
+  const carouselRef = useRef(null); // new ref for carousel container
+
   return (
     <div className="Projects">
-      <div className="FirstProject">
+      <div className="FirstProject" ref={firstProjectRef}>
         <h1>Projects</h1>
         <p>
           Projects page is currently undergoing a large overhaul.<br/><br/>
@@ -24,10 +28,10 @@ export default function Projects() {
         <div className="graph-container">
           <CovidGraphic />
         </div>
-        
+
         {/* Embed carousel inside opaque content box */}
-        <div style={{ marginTop: "30px" }}>
-          <ProjectCarousel projects={projectData} />
+        <div style={{ marginTop: "30px" }} ref={carouselRef}>
+          <ProjectCarousel projects={projectData} parentRef={firstProjectRef} carouselRef={carouselRef} />
         </div>
       </div>
     </div>
