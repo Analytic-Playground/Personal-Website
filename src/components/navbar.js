fe83566ea4f8248835cdd54d8112c5cec77dd552
Matt Krieger <makrieger212@gmail.com>
2026-05-30 17:28:10 -0600

    feat: overlay navbar, pipeline infographic styles, recovered map toggle and date filters, wider desktop boxes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,25 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import '../styles/navbar.css';
 
-// version 01
+// version 02
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
 
-  // Toggle function to show/hide the menu
-  // const toggleMenu = () => {
-  //   setIsOpen(!isOpen);
-  // };
   return (
     <nav className="navbar">
       <ul className="nav-links">
-        <li><a href="/home">Home</a></li>
-        {/* <li><a href="/aboutme">About Me</a></li> */}
-        <li><a href="/resume">Resume</a></li>
-        <li><a href="/projects">Projects</a></li>
-        {/* <li><a href="/blog">Blog</a></li> */}
-        {/* <li><a href="/contact">Contact</a></li> */}
+        <li><a href="/home" className={location.pathname === '/home' ? 'active' : ''}>Home</a></li>
+        <li><a href="/resume" className={location.pathname === '/resume' ? 'active' : ''}>Resume</a></li>
+        <li><a href="/projects" className={location.pathname === '/projects' ? 'active' : ''}>Projects</a></li>
       </ul>
     </nav>
   );
 }
+
 export default Navbar;
